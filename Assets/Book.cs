2f84df3823398d29d307cb45b7820c3060c24e71
GerardCoquard <gerard.coquard.franco@gmail.com>
2023-04-23 19:08:45 +0200

    humo vfx doing

---
+++ Assets/Book.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Book : MonoBehaviour
+{
+    GameObject bookGraphics;
+    GameObject objectHolder;
+    Animator anim;
+    
+
+}
