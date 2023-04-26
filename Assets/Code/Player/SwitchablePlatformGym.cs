a71bd1fcdca9fcd4d5d7bb734beda2f564f5d621
HectorVergel <hvergel@edu.tecnocampus.cat>
2023-04-26 16:11:36 +0200

    platform activation

---
+++ Assets/Code/Player/SwitchablePlatformGym.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SwitchablePlatformGym : MonoBehaviour
+{
+    [SerializeField] private Transform finalPlatformPosition;
+    [SerializeField] private float speed;
+
+
+    public void SetPlatformToNewPosition()
+    {
+        StartCoroutine(MovePlatform());
+    }
+
+    IEnumerator MovePlatform()
+    {
+        float distanceBetweenPositions = (finalPlatformPosition.position - transform.position).magnitude;
+        while (Mathf.Abs(distanceBetweenPositions) > 0.1f)
+        {
+            transform.position = Vector3.Lerp(transform.position, finalPlatformPosition.position, speed * Time.deltaTime);
+            yield return null;
+        }
+    }
+
+}
