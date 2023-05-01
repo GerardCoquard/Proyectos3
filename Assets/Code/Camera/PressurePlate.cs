87ec43f4cc26ef5ae06fcd61644d6ff41772a56d
HectorVergel <hvergel@edu.tecnocampus.cat>
2023-05-01 17:01:17 +0200

    just pp class

---
+++ Assets/Code/Camera/PressurePlate.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class PressurePlate : MonoBehaviour
+{
+    public Transform origin;
+    public LayerMask mask;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+
+    }
+
+    private bool IsSomethingAbove(out RaycastHit collider)
+    {
+        Ray r = new Ray(origin.position, Vector3.up);
+        RaycastHit hit;
+
+        if (Physics.Raycast(r, out hit, 1f, mask))
+        {
+            collider = hit;
+            return true;
+        }
+        else
+        {
+            collider = hit;
+            return false;
+        }
+    }
+}
+
