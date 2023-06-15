8d53c6cba5773a065d0ae6949d1f70930d26333e
HectorVergel <hvergel@edu.tecnocampus.cat>
2023-06-15 22:15:56 +0200

    cosas

--- Assets/DoorAudios.cs
+++ Assets/DoorAudios.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class DoorAudios : MonoBehaviour
 {
-   
+    private bool triggered = false;
    public void DoorSound()
     {
+        if (triggered) return;
         AudioManager.Play("closingDoor" + Random.Range(1, 6).ToString()).Volume(0.5f);
+        triggered = true;
     }
 }
