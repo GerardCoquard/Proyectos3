3783b0634535eae1bdfb16a5514196d192013def
GerardCoquard <gerard.coquard.franco@gmail.com>
2023-06-15 20:24:15 +0200

    epa sonido

--- Assets/Code/LightBeam/LightEmiter.cs
+++ Assets/Code/LightBeam/LightEmiter.cs
@@ -43,7 +43,7 @@ public class LightEmiter : MonoBehaviour
         }
         else
         {
-            sound = AudioManager.Play("rayLoop").SpatialBlend(transform.position,10).FadeIn(2,0.3f).Loop(true);
+            sound = AudioManager.Play("rayLoop").SpatialBlend(transform.position,10).FadeIn(2,0.1f).Loop(true);
         }
     }
     IEnumerator LowerBeamAlpha()
