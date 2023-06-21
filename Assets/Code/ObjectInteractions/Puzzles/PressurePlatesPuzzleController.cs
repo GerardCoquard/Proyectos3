bd8c330387517e0460a3b721673cf41aa26123c3
GerardCoquard <gerard.coquard.franco@gmail.com>
2023-06-21 03:48:01 +0200

    nuevas platafformas i mas feedback en puzzle placas de presison final

--- Assets/Code/ObjectInteractions/Puzzles/PressurePlatesPuzzleController.cs
+++ Assets/Code/ObjectInteractions/Puzzles/PressurePlatesPuzzleController.cs
@@ -7,8 +7,19 @@ public class PressurePlatesPuzzleController: MonoBehaviour
     [SerializeField] List<PressurePlateFixed> plates;
     [SerializeField] UnityEvent eventWhenComplete;
     private bool eventInvoked = false;
+    public ChangeEmission triangle;
+    public ChangeEmission box;
+    public ChangeEmission circle;
+    [ColorUsage(true, true)]
+    public Color minIntensity;
+    [ColorUsage(true, true)]
+    public Color maxIntensity;
 
-    
+    private void Start() {
+        SetBox(false);
+        SetCircle(false);
+        SetTriangle(false);
+    }
     public void CheckIfComplete()
     {
         foreach (PressurePlateFixed plate in plates)
@@ -20,6 +31,21 @@ public class PressurePlatesPuzzleController: MonoBehaviour
             eventWhenComplete?.Invoke();
             eventInvoked = true;
         } 
+    }
+    public void SetTriangle(bool pressed)
+    {
+        if(pressed) triangle.SetEmission(maxIntensity);
+        else triangle.SetEmission(minIntensity);
+    }
+    public void SetBox(bool pressed)
+    {
+        if(pressed) box.SetEmission(maxIntensity);
+        else box.SetEmission(minIntensity);
+    }
+    public void SetCircle(bool pressed)
+    {
+        if(pressed) circle.SetEmission(maxIntensity);
+        else circle.SetEmission(minIntensity);
     }
 
 }
