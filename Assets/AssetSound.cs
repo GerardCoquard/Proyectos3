2876695a5ac3e59fe4e2bce0c9668b43df302b95
HectorVergel <hvergel@edu.tecnocampus.cat>
2023-06-22 00:40:13 +0200

    ya

--- Assets/AssetSound.cs
+++ Assets/AssetSound.cs
@@ -6,8 +6,9 @@ public class AssetSound : MonoBehaviour
 {
     public string soundName = "televisionAudio";
     public float volume = 1f;
+    public bool loop;
     private void Start()
     {
-        AudioManager.Play(soundName).SpatialBlend(transform.position, 15f).Volume(volume);
+        AudioManager.Play(soundName).SpatialBlend(transform.position, 15f).Volume(volume).Loop(loop);
     }
 }
